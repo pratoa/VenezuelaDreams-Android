62a3e42947a199632f7fd8d39d81c914deac4d88
Andres Prato <prato.andres@gmail.com>
2018-07-31 17:50:07 -0400

    Added UI of donation. Created bundle to receive bundle information

--- app/src/main/java/com/example/andresprato/venezueladream/DonationActivity.java
+++ app/src/main/java/com/example/andresprato/venezueladream/DonationActivity.java
@@ -1,5 +1,20 @@
 package com.example.andresprato.venezueladream;
 
-public class DonationActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
+public class DonationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle bundle = this.getIntent().getExtras();
+        String name = bundle.getString("name");
+        int age = bundle.getInt("age");
+        String child_id = bundle.getString("child_id");
+        String url = bundle.getString("image_url");
+
+    }
 }
