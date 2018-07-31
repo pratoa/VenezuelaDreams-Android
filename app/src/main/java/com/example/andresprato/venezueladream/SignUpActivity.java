6b94fddfaca2251fa9d1734a2979cf0069a9d6a3
cains730 <36304269+cains730@users.noreply.github.com>
2018-07-31 11:27:57 -0400

    New

--- app/src/main/java/com/example/andresprato/venezueladream/SignUpActivity.java
+++ app/src/main/java/com/example/andresprato/venezueladream/SignUpActivity.java
@@ -1,4 +1,112 @@
 package com.example.andresprato.venezueladream;
 
-public class SignUpActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUpActivity extends AppCompatActivity {
+    private EditText name, email_id, passwordcheck;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "";
+    private ProgressBar progressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+        TextView btnSignUp = (TextView) findViewById(R.id.login_page);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        email_id = (EditText) findViewById(R.id.input_email);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        passwordcheck = (EditText) findViewById(R.id.input_password);
+        Button ahsignup = (Button) findViewById(R.id.btn_signup);
+
+
+        ahsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = email_id.getText().toString();
+                String password = passwordcheck.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter Email Id", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                progressBar.setVisibility(View.GONE);
+
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                    Map<String,String> myMap =new HashMap<String, String>();
+
+
+                                    myMap.put("email",email);
+                                    myMap.put("registration_type","email");
+                                    myMap.put("admin","false");
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+
+                            }
+
+
+                        });
+            }
+        });
+
+
+    }
 }
+
